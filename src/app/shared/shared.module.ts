fb640c358ba9430a246db73241a0d714b6b3f148
ANDI SHTREPI <andi.shtrepi@live.com>
2020-04-07 00:37:33 +0200

    rr

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import {
-  MatInputModule,
-  MatButtonModule,
-  MatDialogModule,
-  MatCardModule,
-  MatIconModule,
-  MatDatepickerModule,
-  MatNativeDateModule
- } from '@angular/material';
+
 
 
 @NgModule({
@@ -22,14 +14,7 @@ import {
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule,
-    MatInputModule,
-  MatButtonModule,
-  MatDialogModule,
-  MatCardModule,
-  MatIconModule,
-  MatDatepickerModule,
-  MatNativeDateModule, 
+    RouterModule
   
   ],
   exports: [
@@ -37,14 +22,7 @@ import {
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule,
-    MatInputModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatCardModule,
-    MatIconModule,
-    MatDatepickerModule,
-    MatNativeDateModule
+    RouterModule
   ]
 })
 export class SharedModule { }
